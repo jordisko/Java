37718680bfaa905c42a4b3741f6d24d4867ddf24
jordi campos <jordicampos.97@gmail.com>
2022-08-23 12:58:39 +0200

    Ejercicio 10 finalizado

---
+++ Ejercicio10/Ejercicio10Java.java
@@ -0,0 +1,23 @@
+
+public class Ejercicio10Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int num1 = 10-5;
+	System.out.println(num1);
+	int num2 = 55+45;
+	System.out.println(num2);
+	int num3 = -3;
+	System.out.println(num3);
+	int num4 = 5*5;
+	System.out.println(num4);
+	int num5 = 25/7;
+	System.out.println(num5);
+	double num6 = 25/7;
+	System.out.println(num6);
+	int num7 = 25%4;
+	System.out.println(num7);
+	
+	}
+
+}
