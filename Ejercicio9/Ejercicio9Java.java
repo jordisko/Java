944da11a22bf53bb8d4b06127908bc20c665794d
jordi campos <jordicampos.97@gmail.com>
2022-08-23 12:47:15 +0200

    Ejercicio 9 terminado

---
+++ Ejercicio9/Ejercicio9Java.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio9Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String nombre = "Jordi";
+	String apellido = "Campos";
+	String calle = "fabra i puig";
+	String municipio ="Barcelona";
+	String codigo_postal="08031";
+	char sexo = 'm';
+	String correo_electronico = "jordicampos.97@gmail.com";
+	System.out.println(nombre+"\t"+apellido+"\n"+calle+ "\t"+municipio+"\t"+codigo_postal+"\n"+sexo+"\n"+correo_electronico);
+	}
+
+}
