647b3b62658598f984677ff713f0fab936527552
jordi campos <jordicampos.97@gmail.com>
2022-08-23 12:23:40 +0200

    ejercicio 7 finalizado

---
+++ Ejercicio7/Ejercicio7Java.java
@@ -0,0 +1,34 @@
+
+public class Ejercicio7Java {
+
+	final public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	// lógico
+	final boolean boleano = true;
+	// caracter
+	final char caracter = 'j';
+	// Numericos enteros
+	final byte numpequeño = 8;
+	final short nummediano = 288;
+	final int numeronomal = 49992;
+	final long numerogrande = 12291519573259l;
+	// Numerico real
+	final float numeroflotante = 21.12f;
+	final double numerodoble = 21.82181;
+	// Contantes
+	final String nombre = "jordi";
+	System.out.println(boleano);
+	System.out.println(caracter);
+	System.out.println(numpequeño);
+	System.out.println(nummediano);
+	System.out.println(numeronomal);
+	System.out.println(numerogrande);
+	System.out.println(numeroflotante);
+	System.out.println(numerodoble);
+	System.out.println(nombre);
+
+
+
+	}
+
+}
