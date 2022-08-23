9665b9fce9ea970d007a2910500fcb06ec27a3e2
jordi campos <jordicampos.97@gmail.com>
2022-08-23 12:06:33 +0200

    Ejercicio 6 finaliado

---
+++ Ejercicio6/Ejercicio6Java.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio6Java {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	byte num1 = (byte)132;
+	System.out.println(num1);
+	short num2 = (short)32792;
+	System.out.println(num2);
+	int num3 = (int)2147484647l;
+	System.out.println(num3);
+	long num4 = (long)9223372036854795807f;
+	System.out.println(num4);
+	}
+}
