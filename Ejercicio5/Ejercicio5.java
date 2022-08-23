0d12314d225e63e0458d87d605c2c5fff9fcf93b
jordi campos <jordicampos.97@gmail.com>
2022-08-23 12:19:25 +0200

    Ejercicio5 finalizado

---
+++ Ejercicio5/Ejercicio5.java
@@ -0,0 +1,34 @@
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	// lógico
+	boolean boleano = true;
+	// caracter
+	char caracter = 'j';
+	// Numericos enteros
+	byte numpequeño = 8;
+	short nummediano = 288;
+	int numeronomal = 49992;
+	long numerogrande = 12291519573259l;
+	// Numerico real
+	float numeroflotante = 21.12f;
+	double numerodoble = 21.82181;
+	// Contantes
+	final String nombre = "jordi";
+	System.out.println(boleano);
+	System.out.println(caracter);
+	System.out.println(numpequeño);
+	System.out.println(nummediano);
+	System.out.println(numeronomal);
+	System.out.println(numerogrande);
+	System.out.println(numeroflotante);
+	System.out.println(numerodoble);
+	System.out.println(nombre);
+
+
+
+	}
+
+}
