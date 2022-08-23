5c1893014356af03f5127ddcae94683d230867d7
jordi campos <jordicampos.97@gmail.com>
2022-08-23 10:56:47 +0200

    ejercicio 3 finalizado 2 cambio de nombre

---
+++ Ejercicio3/Ejercicio3Java.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hola\nMundo");
+	System.out.println("Hola\tMundo");
+	System.out.println("Hola\\Mundo");
+	System.out.println("Hola\"Mundo\"");
+	System.out.println("Hola\rMundo");
+	System.out.print("Hola\bMundo");
+	}
+
+}
