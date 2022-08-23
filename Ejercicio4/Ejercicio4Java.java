d3de7f9c50a880a39ef8993fa8c9ea96b288741d
jordi campos <jordicampos.97@gmail.com>
2022-08-23 11:04:09 +0200

    Ejercicio4 terminado

---
+++ Ejercicio4/Ejercicio4Java.java
@@ -0,0 +1,12 @@
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int num1 = 10; //Defino la variable num1 como un int y le da valor 10
+	int num2 = 20; //Defino la variable num2 como un int y le da valor 20 
+	int suma = num1 + num2; //Defino la varialbe suma como el resultado de la suma de int1 y int2
+	System.out.println(suma); // Devulevo e imprimo suma por pantalla
+	}
+
+}
