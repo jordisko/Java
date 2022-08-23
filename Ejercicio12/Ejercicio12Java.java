c920088ad9afa1e19dbe41a36229d43cc0c1e4d8
jordi campos <jordicampos.97@gmail.com>
2022-08-23 13:14:35 +0200

    Ejercicio12 finalizado

---
+++ Ejercicio12/Ejercicio12Java.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio12Java {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int num = 25;
+	num +=5;
+	num -= 15;
+	num *=2;
+	num /=2;
+	num %=3;}
+}
