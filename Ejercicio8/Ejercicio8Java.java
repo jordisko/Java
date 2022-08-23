0d0f2136c81983a7c2533a618aa9d28de235b771
jordi campos <jordicampos.97@gmail.com>
2022-08-23 12:53:18 +0200

    Ejercicio 8 finalizado

---
+++ Ejercicio8/Ejercicio8Java.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio8Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int num1 = (int) Math.pow(2, 8);
+	System.out.println("El rango de byte es : "+ num1/2);
+	int num2 = (int) Math.pow(2, 16);
+	System.out.println("El rango de short es : "+ num2/2);
+	long num3 = (long) Math.pow(2, 32);
+	System.out.println("El rango de int es : "+ num3/2);
+	long num4 = (long) Math.pow(2, 64);
+	System.out.println("El rango de long es : "+ num4/2);
+	}
+
+}
