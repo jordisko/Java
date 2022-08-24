460c619d2c1a9ecbf1c9aa0776c43da5ed37c7f0
jordi campos <jordicampos.97@gmail.com>
2022-08-24 10:20:50 +0200

    Ejercicio15 finalizado

---
+++ Ejercicio15/Ejercicio15Java.java
@@ -0,0 +1,21 @@
+
+public class Ejercicio15Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int numero = 8;
+	int result = numero %2;
+	if (result == 0) {
+		System.out.println("El numero es par");
+	}
+	else
+		System.out.println("El numero es impar");
+	double result2 = Math.random();
+	if ((1-result2)>=0.5) {
+		System.out.println("Blanco");
+	}
+	else
+		System.out.println("Negro");
+	}
+
+}
