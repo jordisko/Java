5edacd77c8341980b4f46ef75d11cc49735cb56c
jordi campos <jordicampos.97@gmail.com>
2022-08-24 12:42:06 +0200

    Ejercicio17 finalizado

---
+++ Ejercicio17/Ejercicio17Java.java
@@ -0,0 +1,64 @@
+
+public class Ejercicio17Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int numero = 8;
+	switch (numero) {
+		case 1:
+			if (numero ==1) {
+				System.out.println("uno");
+			}
+		break;
+		case 2:
+			if (numero ==2) {
+				System.out.println("dos");
+			}
+		break;
+		case 3:
+			if (numero ==3) {
+				System.out.println("tres");
+			}
+		break;
+		case 4:
+			if (numero ==4) {
+				System.out.println("cuatro");
+			}
+		break;
+		case 5:
+			if (numero ==5) {
+				System.out.println("cinco");
+			}
+		break;
+		case 6:
+			if (numero ==6) {
+				System.out.println("seis");
+			}
+		break;
+		case 7:
+			if (numero ==7) {
+				System.out.println("siete");
+			}
+		break;
+		case 8:
+			if (numero ==8) {
+				System.out.println("ocho");
+			}
+		break;
+		case 9:
+			if (numero ==9) {
+				System.out.println("nueve");
+			}
+		break;
+		}
+//2
+		double number2= -1;
+		String resultado = (number2>=0)?(number2==0)?"Es cero, na de ná!":"¡Es positivo!":"¡Es negativo!";
+		System.out.println(resultado);
+
+		
+		}
+	
+	
+
+}
