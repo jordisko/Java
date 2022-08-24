ddb97997201bdd49c4747cc3bddad998b719b728
jordi campos <jordicampos.97@gmail.com>
2022-08-24 10:09:40 +0200

    Ejercicio14 finalizado

---
+++ Ejercicio14/Ejercicio14Java.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio14Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	boolean bool1=true, bool2= false, bool3 = false;
+	System.out.println(bool1 == !bool2);
+	System.out.println(bool2 == !bool3);
+	}
+
+}
