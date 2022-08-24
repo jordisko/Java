19258bdddc0bc90d252883c47ed26316a7b6fca3
jordi campos <jordicampos.97@gmail.com>
2022-08-24 09:50:02 +0200

    Ejercicio11 finalizado

---
+++ Ejercicio11/Ejercicio11Java.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio11Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int numero = 0;
+	++numero;
+	System.out.println(numero);
+	int numero2 =20;
+	numero2-=2;
+	System.out.println(numero2);
+	}
+
+}
