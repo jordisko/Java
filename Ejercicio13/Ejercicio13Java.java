339e4114f951a03d7ff3d11d4b48bd08da6f8eb3
jordi campos <jordicampos.97@gmail.com>
2022-08-24 10:06:13 +0200

    Ejercicio13 finalizado

---
+++ Ejercicio13/Ejercicio13Java.java
@@ -0,0 +1,24 @@
+
+public class Ejercicio13Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int num1=1,num2=2,num3=3,num4=4;
+	System.out.println("Es num4 igual a num2 ?" + (num4==num2));
+	System.out.println("Es num2 igual a num3 ?" + (num2 ==num3));
+	System.out.println("Es num1 distinto a num3 ?" + (num1 !=num3));
+	System.out.println("Es num2 distinto a num2 ?" + (num2 != num2));
+	System.out.println("Es num4 menor que num3 ?" + (num4 < num3));
+	System.out.println("Es num3 menor que num1 ?" + (num3 < num1));
+	System.out.println("Es num1 mayor que num3 ?" + (num1 > num3));
+	System.out.println("Es num2 mayor que num4 ?" + (num2 > num4));
+	System.out.println("Es num2 mayor que num3 ?" + (num2 > num3));
+	System.out.println("Es num2 igual o menor que num3 ?" + (num2 <= num3));
+	System.out.println("Es num2 igual o menor que num4 ?" + (num2 <= num4));
+	System.out.println("Es num2 igual o menor que num1 ?" + (num2 <= num1));
+	System.out.println("Es num1 mayor o igual que num2 ?" + (num1 >= num2));
+	System.out.println("Es num2 mayor o igual que num2 ?" + (num2 >= num2));
+	
+	}
+
+}
