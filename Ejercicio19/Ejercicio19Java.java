a9a19241b16d0b899ff459bdc8c20b02bc8ae055
jordi campos <jordicampos.97@gmail.com>
2022-08-25 10:30:22 +0200

    Ejercicio19 falta el ultimo apartado

--- Ejercicio19/Ejercicio19Java.java
+++ Ejercicio19/Ejercicio19Java.java
@@ -3,9 +3,10 @@ import java.lang.reflect.Array;
 public class Ejercicio19Java {
 
 	public static void main(String[] args) {
-		//NumerosPares();
-		int[] arr = {3,6,9,10,12,15,14};
-		divisiblesDe3(arr);
+		//1// NumerosPares();
+		//2// int[] arr = {3,6,9,10,12,15,14};
+		//2// divisiblesDe3(arr);
+		//3//saludos(4);
 	}
 
 	// 1
@@ -21,12 +22,36 @@ public class Ejercicio19Java {
 	public static int[] divisiblesDe3(int[] arr) {
 		String arrTmp = "";
 		for (int item : arr) {
-			if(item%3==0) {
-				arrTmp += String.valueOf(item);
-				System.out.println(arrTmp);	
+			if (item % 3 == 0) {
+				arrTmp = String.valueOf(item);
 			}
+			System.out.println(arrTmp);
+
 		}
-	
+
 		return null;
+	}
+
+	// 3
+	public static void saludos(int numerodeveces) {
+		int contador1 = 0;
+		do {
+			System.out.println("Hola");
+			contador1++;
+		} while (contador1 <= numerodeveces);
+	}
+	//4
+	public static void eliminandoAbecedario() {
+		String abecedario= "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		String guardado = "";
+		StringBuilder abecedarioalreves = new StringBuilder(abecedario);
+		abecedario = abecedarioalreves.reverse().toString();
+		for (char i :abecedario {
+			guardado += (abecedario , abecedario.length();
+			abecedario = abecedario(0,abecedario.length()-1);
+			guardado += 
+			
+			
+		}
 	}
 }
